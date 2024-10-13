0276b0a040065fc3efd1334a559bf0ace833586e
brad@smithwebtek.com <brad@smithwebtek.com>
2024-10-12 22:08:43 -0400

    Add rake db:seed to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,4 +75,11 @@ task :migrate_db do
   end
 end
 
+namespace :deploy do
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
 after 'deploy:finishing', 'deploy:cleanup_with_sudo'
