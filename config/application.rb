a7a05f8eeed2f701e7e235ebec3cebb6f5a9c816
brad@smithwebtek.com <brad@smithwebtek.com>
2024-10-12 21:22:27 -0400

    Fix up application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,51 @@
-require_relative "boot"
+# require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+
+# # Require the gems listed in Gemfile, including any gems
+# # you've limited to :test, :development, or :production.
+# Bundler.require(*Rails.groups)
+
+# module MusicStudioApi
+#   class Application < Rails::Application
+#     # Initialize configuration defaults for originally generated Rails version.
+#     config.load_defaults 5.2
+    
+#     # Please, add to the `ignore` list any other `lib` subdirectories that do
+#     # not contain `.rb` files, or that should not be reloaded or eager loaded.
+#     # Common ones are `templates`, `generators`, or `middleware`, for example.
+#     # config.autoload_lib(ignore: %w[assets tasks])
+#     config.autoload_paths += %W(#{config.root}/lib)
+    
+#     # Configuration for the application, engines, and railties goes here.
+#     #
+#     # These settings can be overridden in specific environments using the files
+#     # in config/environments, which are processed later.
+#     #
+#     # config.time_zone = "Central Time (US & Canada)"
+#     # config.eager_load_paths << Rails.root.join("extras")
+
+#     # Only loads a smaller set of middleware suitable for API only apps.
+#     # Middleware like session, flash, cookies can be added back manually.
+#     # Skip views, helpers and assets when generating a new resource.
+#     config.api_only = true
+#   end
+# end
+
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,20 +55,11 @@ module MusicStudioApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w[assets tasks])
-    config.autoload_paths += %W(#{config.root}/lib)
-    
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
